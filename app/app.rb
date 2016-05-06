66d5f21d1bc2bee01723708a106e0bc1c21da01c
Ben Stein <bumstain@gmail.com>
2016-05-07 03:12:41 +1000

    Partial solution for sign out - test fails, but works in browser

--- app/app.rb
+++ app/app.rb
@@ -10,11 +10,18 @@ require_relative 'models/tag'
 
 class BookmarkManager < Sinatra::Base
 
+  use Rack::MethodOverride
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
-  register Sinatra::Flash
   
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+  
   get '/links' do
     @links = Link.all
     erb :'links/index'
@@ -47,14 +54,20 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/sessions' do 
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      session[:user_id] = @user.id
       redirect to('/links')
     else
       flash.now[:errors] = ['The email or password is incorrect']
       erb :'sessions/new'
     end
+  end
+
+  delete '/sessions' do
+    session[:user_id] = nil
+    flash.keep[:notice] = 'goodbye!'
+    redirect to '/links'
   end
 
   get '/tags/:name' do
@@ -70,12 +83,6 @@ class BookmarkManager < Sinatra::Base
     end
     link.save
   	redirect '/links'
-  end
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
   end
 
     # start the server if ruby file executed directly
